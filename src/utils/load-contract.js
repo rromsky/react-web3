6ebc15c997fa2d4c9f49261758a8c4d3e5433308
rromsky <rroma5129@gmail.com>
2023-10-12 13:16:34 +0300

    ads

--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -8,7 +8,7 @@ export const loadContract = async (name, provider) => {
   if (Artifact.networks[NETWORK_ID].address) {
     const contract = new provider.eth.Contract(
       Artifact.abi,
-      Artifact.networks[NETWORK_ID].address
+      "0xf018D0FcE830eb2CC64d8240Fd4b30363903313E"
     );
     return contract;
   } else {
